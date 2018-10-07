c4f9522b0cdab32acf1b3533f21724b82a75edd6
笠井　光 <01018603@CA2606.local>
2018-10-07 23:11:25 +0900

    devise view きれいに

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
       registrations: "users/registrations",
-      confirmations: "users/confirmations"
+      confirmations: "users/confirmations",
+      sessions: "users/sessions",
+      passwords: "users/passwords"
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
